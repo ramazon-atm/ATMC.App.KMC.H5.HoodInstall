bfc37f9d70c1882b464758cd61b669950061193b
SHIM1999 <gbpforfuture@gmail.com>
2026-06-24 14:33:45 +0900

    카테고리 버그 수정했음

--- ATMC.App.KMC.H5.HoodInstall/ModelConfig.cs
+++ ATMC.App.KMC.H5.HoodInstall/ModelConfig.cs
@@ -20,10 +20,11 @@ namespace ATMC.App.KMC.H5.HoodInstall {
     public class ModelConfig : ModelConfigBase {
         [Category("REGISTRATION")]
         public FastRegistrationConfig PickReg { get; set; } = new FastRegistrationConfig();
+        [Category("REGISTRATION")]
         public FastRegistrationConfig Pick_degReg { get; set; } = new FastRegistrationConfig();
         [Category("REGISTRATION")]
         public FastRegistrationConfig InstallReg { get; set; } = new FastRegistrationConfig();
-        [Category("REGISTRATION")]
+        
         //TODO: add more registration configs if needed 
 
         //Define registration keys
